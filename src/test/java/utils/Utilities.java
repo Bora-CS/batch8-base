953c8a9c86e0fb45ef9b793fb6ecb15d8078cc6a
Anna Zhao <110501571+AnnaZhao0209@users.noreply.github.com>
2022-08-18 19:34:27 -0400

    added new line

--- src/test/java/utils/Utilities.java
+++ src/test/java/utils/Utilities.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Utilities {
 
+	public static final String name ="Anna";
 	WebDriver driver;
 	private static String mainHandle = null;
 
