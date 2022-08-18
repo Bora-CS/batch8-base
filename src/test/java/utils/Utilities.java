af81daba4a005852ed0a33a730e22177e98523a6
Eva <gao8180904@gmail.com>
2022-08-18 19:39:39 -0400

    add

--- src/test/java/utils/Utilities.java
+++ src/test/java/utils/Utilities.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Utilities {
+	public static String name ="eva";
 	private static String mainHandle = null;
 
 	public static void wait(int seconds) {
