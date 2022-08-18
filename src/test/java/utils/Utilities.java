8f13ff69dedfcfa8e984bf5b1d3cc90218c4f4dc
CarterCChen <110501129+CarterCChen@users.noreply.github.com>
2022-08-18 19:59:23 -0400

    edited new code

--- src/test/java/utils/Utilities.java
+++ src/test/java/utils/Utilities.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Utilities {
 	
-	public static final String name = "Tae Lee 2";
+	public static final String name = "carter chen";
 	
 	WebDriver driver;
 	private static String mainHandle = null;
