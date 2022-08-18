fbc00e0a28bb874f450b887af0b983bb233add35
Awzar Mahmut <Awzar1993@gmail.com>
2022-08-18 19:15:10 -0400

    Name

--- src/test/java/utils/Utilities.java
+++ src/test/java/utils/Utilities.java
@@ -12,7 +12,13 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
+
 public class Utilities {
+	
+	static final String name = "Awzar";
+	
+	
 
 	WebDriver driver;
 	private static String mainHandle = null;
