3ea839aa65d3db156b92de625adc4a5ea36240ac
Sunny Chen <sunnychen@Sunnys-Air.fios-router.home>
2022-08-18 21:15:11 -0400

    eieee

--- src/test/java/utils/Utilities.java
+++ src/test/java/utils/Utilities.java
@@ -14,8 +14,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Utilities {
 
-	public static final String lixiaName = "Lixia";
-	public static final String kaerName = "Kaer";
+	
 
 	WebDriver driver;
 	private static String mainHandle = null;
