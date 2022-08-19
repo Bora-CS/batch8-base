69991a9e88c8f1d1858f8e7a75a322d1a5e84c20
Alex Chen <aspchen805@gmail.com>
2022-08-18 20:09:56 -0400

    change name

--- src/test/java/utils/Utilities.java
+++ src/test/java/utils/Utilities.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Utilities {
 
-	public static final String name = "Tae Lee 2";
+	public static final String name = "Alex Chen";
 
 	WebDriver driver;
 	private static String mainHandle = null;
