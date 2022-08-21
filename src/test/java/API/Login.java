633ddc5bdbbee617e14dc5addddccf04b949a213
Lixia Sun <110501221+LixiaSun@users.noreply.github.com>
2022-08-21 09:52:54 -0400

    Update Login.java

--- src/test/java/API/Login.java
+++ src/test/java/API/Login.java
@@ -1,42 +1,39 @@
-package API;
-
-import java.util.HashMap;
-
+package api;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import pojo.LoginRequestBody;
+import pojo.LoginResponseBody;
 
 public class Login {
 
 	public static void main(String[] args) {
-		
-		RestAssured.baseURI = "https://boratech.herokuapp.com/";
-		String endPoint = "/api/auth";
+
+		RestAssured.baseURI = "https://boratech.herokuapp.com";
+		String endpoint = "/api/auth";
 		RequestSpecification request = RestAssured.given();
-		
-		request.header("Content-Type","application/json");
-		
-		HashMap<String,String> body = new HashMap<String,String>();
-		body.put("email", "lixiasun2000@gmail.com");
-		body.put("password", "Lsun1688");
-		
+
+		request.header("Content-Type", "application/json");
+
+		LoginRequestBody body = new LoginRequestBody("muradil.erkin@boratechschool.com", "Boratech");
 		request.body(body);
-		
-		Response response = request.post(endPoint);
+
+		Response response = request.post(endpoint);
 		int actualStatusCode = response.getStatusCode();
-		int expectStatusCode = 200;
-		
-		if (expectStatusCode == actualStatusCode) {
-			String responseBody = response.getBody().asString();
-			System.out.println(responseBody);
-			
-			System.out.println("Test passed");
-		}else {
-			System.out.println("Test failed");
-			System.out.println("Expect Status Code: " + expectStatusCode );
-			System.out.println("Actual Status Code: " + actualStatusCode );
+		int expectedStatusCode = 200;
+
+		if (expectedStatusCode == actualStatusCode) {
+			LoginResponseBody responseBody = response.getBody().as(LoginResponseBody.class);
+			String token = responseBody.getToken();
+			System.out.println("Token: " + token);
+			System.out.println("Test Passed");
+		} else {
+			System.out.println("Test Failed");
+			System.out.println("Expected Status Code: " + expectedStatusCode);
+			System.out.println("Actual Status Code: " + actualStatusCode);
 		}
+
 	}
 
 }
