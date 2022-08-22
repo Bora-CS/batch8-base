20be0bb94f36d5693578c40665dfd64348c8b190
Anna Zhao <110501571+AnnaZhao0209@users.noreply.github.com>
2022-08-22 14:02:30 -0400

    work day-1
    this is  first testing by desktop mulriple change waiting to push

--- src/test/java/selenium/NewClass.java
+++ src/test/java/selenium/NewClass.java
@@ -4,5 +4,7 @@ public class NewClass {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
+		
+		System.out.println("gitHub desktop testing");
 	}
 }
--- src/test/java/selenium/PracticeForGitHub.java
+++ src/test/java/selenium/PracticeForGitHub.java
@@ -5,6 +5,8 @@ public class PracticeForGitHub {
 	public static void main(String[] args) {
 		System.out.println("Just for practice.");	
 		
+		System.out.println("gitHub desktop");
+		
 		
 	}
 
