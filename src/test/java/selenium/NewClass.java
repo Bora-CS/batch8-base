3fe0092475c8bc9de65e66e4425c9090d3282d94
Anna Zhao <110501571+AnnaZhao0209@users.noreply.github.com>
2022-08-22 14:07:26 -0400

    work-day2
    this is for day 2

--- src/test/java/selenium/NewClass.java
+++ src/test/java/selenium/NewClass.java
@@ -4,7 +4,8 @@ public class NewClass {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
+		//"del line below"
 		
-		System.out.println("gitHub desktop testing");
+		
 	}
 }
