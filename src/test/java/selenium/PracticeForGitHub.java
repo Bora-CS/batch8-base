2f43d3f625d5ba249f2193d2549a68769769d3a4
Anna Zhao <110501571+AnnaZhao0209@users.noreply.github.com>
2022-08-22 14:07:50 -0400

    word day 3
    this is work for day 3

--- src/test/java/selenium/PracticeForGitHub.java
+++ src/test/java/selenium/PracticeForGitHub.java
@@ -5,7 +5,8 @@ public class PracticeForGitHub {
 	public static void main(String[] args) {
 		System.out.println("Just for practice.");	
 		
-		System.out.println("gitHub desktop");
+	
+		//"del line8 above"
 		
 		
 	}
