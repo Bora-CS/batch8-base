ada7c637ad0c3bccd8c818a6122b3ada13b9f385
Anna Zhao <110501571+AnnaZhao0209@users.noreply.github.com>
2022-08-22 22:02:36 -0400

    GithubDesktop Practice
    Delete a blank line

--- src/test/java/githPractice/TestGithubDesktop2.java
+++ src/test/java/githPractice/TestGithubDesktop2.java
@@ -3,7 +3,6 @@ package githPractice;
 public class TestGithubDesktop2 {
 public static void main(String[] args) {
 	System.out.println("testing case 3");
-	
 	System.out.println("testing case3,first change");
 }
 }
