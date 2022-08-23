7641ebc93813e9f5b50403c179e4482e543bc565
Anna Zhao <110501571+AnnaZhao0209@users.noreply.github.com>
2022-08-23 00:16:18 -0400

    boratech-005
    line 7 is new code

--- src/test/java/githPractice/TestGithubDesktop2.java
+++ src/test/java/githPractice/TestGithubDesktop2.java
@@ -4,5 +4,6 @@ public class TestGithubDesktop2 {
 public static void main(String[] args) {
 	System.out.println("testing case 3");
 	System.out.println("testing case3,first change");
+	System.out.println("testing second change");
 }
 }
