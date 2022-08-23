e1fe6c1e736dd3162a26bba34558d8784759356b
Anna Zhao <110501571+AnnaZhao0209@users.noreply.github.com>
2022-08-22 21:41:39 -0400

    BoraTech-001
    work day 1 first change;

---
+++ src/test/java/githPractice/TestGithubDesktop1.java
@@ -0,0 +1,9 @@
+package githPractice;
+
+public class TestGithubDesktop1 {
+public static void main(String[] args) {
+	System.out.println("This is test case 2."
+			+ " make some change at this class");
+	
+}
+}
