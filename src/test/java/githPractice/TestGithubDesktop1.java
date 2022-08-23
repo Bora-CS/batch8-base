f2c7c68e1c888f7e38c0c65ee3db36494ad19cf9
Anna Zhao <110501571+AnnaZhao0209@users.noreply.github.com>
2022-08-23 00:15:32 -0400

    boratech-004
    line 7 is new

--- src/test/java/githPractice/TestGithubDesktop1.java
+++ src/test/java/githPractice/TestGithubDesktop1.java
@@ -4,7 +4,7 @@ public class TestGithubDesktop1 {
 public static void main(String[] args) {
 	System.out.println("This is test case 2."
 			+ " make some change at this class");
-	
+	System.out.println("third  change. add line 7 and line 8. ");
 	System.out.println("second change. del line 7 and line 8. ");
 } 
 }
