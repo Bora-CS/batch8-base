77b4ee63a1a5f705c7cb42ee70720988bc2fcec1
Anna Zhao <110501571+AnnaZhao0209@users.noreply.github.com>
2022-08-22 21:47:28 -0400

    BoraTech --- 002
    work day 2 .del line 7 and line 8

--- src/test/java/githPractice/TestGithubDesktop1.java
+++ src/test/java/githPractice/TestGithubDesktop1.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("This is test case 2."
 			+ " make some change at this class");
 	
-}
+	System.out.println("second change. del line 7 and line 8. ");
+} 
 }
