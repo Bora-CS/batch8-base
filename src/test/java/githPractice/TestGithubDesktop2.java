8ce7098be2b691c52db231309ed14d211cd8bc16
Anna Zhao <110501571+AnnaZhao0209@users.noreply.github.com>
2022-08-22 22:00:17 -0400

    Boratech-003
    add  code 7 at line 7

---
+++ src/test/java/githPractice/TestGithubDesktop2.java
@@ -0,0 +1,9 @@
+package githPractice;
+
+public class TestGithubDesktop2 {
+public static void main(String[] args) {
+	System.out.println("testing case 3");
+	
+	System.out.println("testing case3,first change");
+}
+}
