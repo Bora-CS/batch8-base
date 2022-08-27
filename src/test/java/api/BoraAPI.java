55d03e2330e4ada0992d93739445b9fef199e8ba
Muradil Erkin <miller.muradil@gmail.com>
2022-08-27 12:13:22 -0400

    refactored pojo, created addExperience method

--- src/test/java/api/BoraAPI.java
+++ src/test/java/api/BoraAPI.java
@@ -2,9 +2,15 @@ package api;
 
 import static org.testng.Assert.*;
 
+import java.util.List;
+
+import org.testng.Assert;
+
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import pojo.Experience;
 import pojo.LoginRequestBody;
 import pojo.LoginResponseBody;
 
@@ -31,6 +37,27 @@ public class BoraAPI {
 		assertNotNull(token);
 		assertTrue(token != "");
 		return token;
+	}
+
+	public static List<Experience> addExperience(String token, Experience body) {
+		RestAssured.baseURI = "https://boratech.herokuapp.com";
+		String endpoint = "/api/profile/experience";
+		RequestSpecification request = RestAssured.given();
+
+		request.header("Content-Type", "application/json");
+		request.header("x-auth-token", token);
+
+		request.body(body);
+
+		Response response = request.put(endpoint);
+		int actualStatusCode = response.getStatusCode();
+		int expectedStatusCode = 200;
+
+		Assert.assertEquals(actualStatusCode, expectedStatusCode);
+
+		JsonPath jp = response.jsonPath();
+		List<Experience> experiences = jp.getList("experience", Experience.class);
+		return experiences;
 	}
 
 }
--- src/test/java/apiTest/LoginTest.java
+++ src/test/java/apiTest/LoginTest.java
@@ -1,13 +1,30 @@
 package apiTest;
 
+import java.util.List;
 import api.BoraAPI;
+import pojo.Experience;
+
+import static org.testng.Assert.*;
 
 public class LoginTest {
 
 	public static void main(String[] args) {
 
-		String token = BoraAPI.login("muradil.erkin@boratechschool.com", "Boratech1");
-		System.out.println("Token: " + token);
+		String token = BoraAPI.login("muradil.erkin@boratechschool.com", "Boratech");
+
+		Experience inputExperience = new Experience("Cashier", "Ben Gong's Tea", "Annandale, VA", "2021/8/16", "", true,
+				"Best place to work in Boba Industry", null);
+
+		List<Experience> experiences = BoraAPI.addExperience(token, inputExperience);
+
+		boolean found = false;
+		for (Experience experience : experiences) {
+			if (experience.company.equals(inputExperience.company)) {
+				found = true;
+				break;
+			}
+		}
+		assertTrue(found);
 
 	}
 
